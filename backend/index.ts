0cfc2565914f85480d4678866c982a9ed2dd5b0c
NjugunaBrian <bryunnjuguna300@gmail.com>
2025-02-12 17:36:43 +0300

    feat: Mongodb database connection

--- backend/index.ts
+++ backend/index.ts
@@ -1,8 +1,13 @@
 import express, {Request, Response, NextFunction} from "express";
+import connectDB from "./config/dbConn";
+import mongoose from "mongoose";
 
 const app = express();
 
-const port = process.env.PORT || 4000;
+const PORT = process.env.PORT || 4000;
+
+//connect to MongoDB
+connectDB()
 
 // built-in middleware for json 
 app.use(express.json());
@@ -10,7 +15,8 @@ app.use(express.json());
 app.get('/', (req: Request, res: Response) => {
     res.send('Express + TypeScript Server');
   });
-  
-  app.listen(port, () => {
-    console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
-  });
+
+mongoose.connection.once('open', () => {
+    console.log('Connected to MongoDB');
+    app.listen(PORT, () => console.log(`⚡️[server]: Server is running on port ${PORT}`))
+})  
---
+++ backend/models/User.ts
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+const Schema  = mongoose.Schema;
+
+const userSchema  = new Schema({
+    username: {
+        type: String,
+        required: true,
+    },
+    roles: {
+        User: {
+            type: Number,
+            default: 2001
+        },
+        Editor: Number,
+        Admin: Number
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    refreshToken: [String]
+});
+
+module.exports = mongoose.model('User', userSchema);
