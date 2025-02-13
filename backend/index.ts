4df7e4209463a482b472418aae48bca732199328
NjugunaBrian <bryunnjuguna300@gmail.com>
2025-02-13 14:06:35 +0300

    feat: add static file serving and custom 404 error handling

--- backend/index.ts
+++ backend/index.ts
@@ -1,6 +1,7 @@
 import express, {Request, Response, NextFunction} from "express";
 import connectDB from "./config/dbConn";
 import mongoose from "mongoose";
+import path from "path";
 
 const app = express();
 
@@ -12,9 +13,23 @@ connectDB()
 // built-in middleware for json 
 app.use(express.json());
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('Express + TypeScript Server');
-  });
+
+//serve static files
+app.use('/', express.static(path.join(__dirname, '/public')));
+
+//routes
+
+
+app.all('*', (req, res) => {
+    res.status(404);
+    if(req.accepts('html')){
+        res.sendFile(path.join(__dirname, 'views', '404.html'));
+    } else if(req.accepts('json')){
+        res.json({ 'error': '404 Not Found'});
+    } else {
+        res.type('txt').send("404 Not Found");
+    }
+});
 
 mongoose.connection.once('open', () => {
     console.log('Connected to MongoDB');
